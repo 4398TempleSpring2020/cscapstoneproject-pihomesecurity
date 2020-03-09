7f8d2c345f66edad756d6a5d2ac354d73d73f71e
Dishant Patel <tug02945@temple.edu>
2020-03-09 16:36:53 -0400

    Create Empty Activity to add the login/register activity and the user dashboard and navigate between them

---
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/MainEmptyActivity.java
@@ -0,0 +1,17 @@
+package edu.temple.pihomesecuritymobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_empty);
+        Intent intent;
+
+    }
+}
