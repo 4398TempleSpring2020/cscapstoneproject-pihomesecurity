1315ef7ab883326def5bf9d1fbfafe2c56f0df96
Dishant Patel <tug02945@temple.edu>
2020-03-10 12:39:58 -0400

    added in initial navigation to MainEmptyActivity

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/MainEmptyActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/MainEmptyActivity.java
@@ -2,10 +2,13 @@ package edu.temple.pihomesecuritymobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainEmptyActivity extends AppCompatActivity {
+    SharedPreferences sharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,12 @@ public class MainEmptyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_empty);
         Intent intent;
 
+        if(true){
+            intent = new Intent(this,RegisterActivity.class);
+            startActivity(intent);
+        } else{
+            intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
