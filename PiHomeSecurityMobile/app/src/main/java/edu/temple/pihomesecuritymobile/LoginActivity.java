162c7799fe9591ccc955dbb9dc37f93b36402c2c
Dishant Patel <tug02945@temple.edu>
2020-03-10 13:12:55 -0400

    added titles and button functionality to register activity

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
@@ -10,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("Login");
     }
 }
--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/RegisterActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/RegisterActivity.java
@@ -2,7 +2,12 @@ package edu.temple.pihomesecuritymobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setTitle("Register");
+        EditText homeID = findViewById(R.id.editText3);
+        Button login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button register = findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] form = createRegisterForm();
+                if(form != null){
+
+                }
+            }
+        });
     }
+
+    public String[] createRegisterForm(){
+        EditText homeID = findViewById(R.id.editText4);
+        EditText homeAddr = findViewById(R.id.editText3);
+        EditText userID = findViewById(R.id.editText5);
+        if(homeID.getText().toString().equals("") && homeAddr.getText().toString().equals("") && userID.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Some fields have been left empty", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        Toast.makeText(getApplicationContext(),"Registering", Toast.LENGTH_SHORT).show();
+        String[] registerForm = {homeID.getText().toString(),homeAddr.getText().toString(),userID.getText().toString()};
+        return registerForm;
+    }
+
 }
