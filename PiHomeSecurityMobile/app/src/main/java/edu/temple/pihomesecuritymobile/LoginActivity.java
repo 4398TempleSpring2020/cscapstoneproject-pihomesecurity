a59791be8690ce4e2531c9334c86e68c23e6d48c
Dishant Patel <tug02945@temple.edu>
2020-03-10 14:02:14 -0400

    added button functionality to login

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
@@ -2,14 +2,46 @@ package edu.temple.pihomesecuritymobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    SharedPreferences sharePref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+        sharePref = getSharedPreferences("PREF_NAME",MODE_PRIVATE);
+        Button login = findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] form = createLoginForm();
+                if(form != null){
+                    sharePref.edit().putBoolean("Registered",true).apply();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private String[] createLoginForm(){
+        EditText HomeAccount = findViewById(R.id.editText);
+        EditText password = findViewById(R.id.editText2);
+        if(HomeAccount.getText().toString().equals("") || password.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Some fields have been left empty", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        String[] loginForm = {HomeAccount.getText().toString(), password.getText().toString()};
+        Toast.makeText(getApplicationContext(),"Logging in...", Toast.LENGTH_SHORT).show();
+        return loginForm;
     }
 }
