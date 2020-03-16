3946af5a3753753ce31a029750bd274689be1550
Dishant Patel <tug02945@temple.edu>
2020-03-16 16:01:19 -0400

    Built up DataFragment

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/dashboard/DashboardFragment.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/dashboard/DashboardFragment.java
@@ -24,12 +24,7 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView.setText("Settings Menu");
         return root;
     }
 }
