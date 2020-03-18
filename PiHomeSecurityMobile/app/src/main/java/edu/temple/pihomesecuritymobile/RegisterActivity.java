91d2b762983033abac2fb25940ff3f0f93b2a56a
Danielle Burella <tuj82439@temple.edu>
2020-03-17 20:01:08 -0400

    added new error messages to form fields; made variables for text field data because needed to add trim() and it got too long and complicated

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/RegisterActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/RegisterActivity.java
@@ -62,39 +62,67 @@ public class RegisterActivity extends AppCompatActivity {
         EditText confirm = findViewById(R.id.editText6);
         EditText homePin = findViewById(R.id.pinText);
         EditText phoneNumber = findViewById(R.id.phoneNumText);
+        String newUser = username.getText().toString().trim();
+        String newPass = password.getText().toString().trim();
+        String address = homeAddr.getText().toString().trim();
+        String phone = phoneNumber.getText().toString().trim();
+        String pin = homePin.getText().toString().trim();
         long phoneNum;
         //ensure each field is filled out
-        if(username.getText().toString().equals("") && homeAddr.getText().toString().equals("") && password.getText().toString().equals("")
-                && homePin.getText().toString().equals("") && phoneNumber.getText().toString().equals("")){
+        if(newUser.equals("") && address.equals("") && newPass.equals("") && confirm.getText().toString().trim().equals("")
+                && pin.equals("") && phone.equals("")){
+            if (newUser.equals("")) {
+                username.setError("Field is empty");
+            }
+            if (newPass.equals("")) {
+                password.setError("Field is empty");
+            }
+            if (address.equals("")) {
+                homeAddr.setError("Field is empty");
+            }
+            if (pin.equals("")) {
+                homePin.setError("Field is empty");
+            }
+            if (phone.equals("")) {
+                phoneNumber.setError("Field is empty");
+            }
+            if (confirm.getText().toString().trim().equals("")) {
+                confirm.setError("Field is empty");
+            }
             Toast.makeText(getApplicationContext(),"Some fields have been left empty", Toast.LENGTH_SHORT).show();
             return null;
         }
         //ensure password and confirm password match
-        if(!confirm.getText().toString().equals(password.getText().toString())){
+        if(!confirm.getText().toString().equals(newPass)) {
+            confirm.setError("Passwords do not match");
+            password.setError("Passwords do not match");
             Toast.makeText(getApplicationContext(),"Passwords do not match", Toast.LENGTH_SHORT).show();
             return null;
         }
         //ensure phone number is correct length
-        if (phoneNumber.getText().toString().length()!=10) {
+        if (phone.length()!=10) {
+            phoneNumber.setError("Please enter a phone number like 1231231234");
             Toast.makeText(getApplicationContext(),"Please enter a valid phone number with no dashes or symbols.", Toast.LENGTH_SHORT).show();
             return null;
         }
         try {
             //try to cast phone number to long
-            phoneNum = Long.parseLong(phoneNumber.getText().toString());
+            phoneNum = Long.parseLong(phone);
             //Log.d("Phone number", "" + Long.toString(phoneNum));
         } catch(NumberFormatException nfe) {
+            phoneNumber.setError("Please enter a phone number like 1231231234");
             Toast.makeText(getApplicationContext(),"Please enter a valid phone number.", Toast.LENGTH_SHORT).show();
             Log.d("NumberFormatException", "" + nfe.toString());
             return null;
         }
         //check if home address exists in database, will use LIKE instead of = in lambda so only street address needs to match
-        String result = contentManager.selectIDStatement("HomeAccount", "AccountPin, AccountID, NumOfUsers", "HomeAccountAddress", "'" + homeAddr.getText().toString() + "'");
+        String result = contentManager.selectIDStatement("HomeAccount", "AccountPin, AccountID, NumOfUsers", "HomeAccountAddress", "'" + address + "'");
         Log.d("LOOKUP_ADDRESS", "" + result);
         //put result into a response object
         response = contentManager.makeResponse(result);
         //see if we got a status code signaling home address does not exist
         if (response.getStatusCode()==contentManager.records_not_exist) {
+            homeAddr.setError("Home address not found");
             Toast.makeText(getApplicationContext(),"Home address not registered", Toast.LENGTH_SHORT).show();
             return null;
         } else if (response==null) {
@@ -105,7 +133,8 @@ public class RegisterActivity extends AppCompatActivity {
         int numUsers;
         try {
             //check that the pin number matches what the Home Account Pin equals
-            if(!homePin.getText().toString().equals(response.getBody().getString("AccountPin"))){
+            if(!pin.equals(response.getBody().getString("AccountPin"))){
+                homePin.setError("Pin does not match");
                 Toast.makeText(getApplicationContext(),"Incorrect pin used", Toast.LENGTH_SHORT).show();
                 return null;
             }
@@ -130,7 +159,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         //insert new user into database
         result = contentManager.insertStatement("UserAccounts", "AccountID, Username, UserPassword, UserPhoneNumber, MasterUserFlag, PhoneId",
-                "'" + accountID + "','" + username.getText().toString() + "','" + password.getText().toString() + "'," + phoneNum + ",'" + "1" + "','" + deviceId + "'");
+                "'" + accountID + "','" + newUser + "','" + newPass + "'," + phoneNum + ",'" + "1" + "','" + deviceId + "'");
         //Log.d("result", "insert: " + result);
         //increment number of users for home account
         numUsers +=1;
@@ -138,7 +167,7 @@ public class RegisterActivity extends AppCompatActivity {
         //update home account with new number of users
         result = contentManager.updateStatement("HomeAccount", "NumOfUsers", "'" + numUsers + "'", "AccountID", accountID);
         //Log.d("result", "update: " + result);
-        String[] registerForm = {username.getText().toString(),homeAddr.getText().toString(),password.getText().toString(), homePin.getText().toString()};
+        String[] registerForm = {newUser, address, newPass, pin};
         return registerForm;
     }
 
