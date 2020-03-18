09aa2be6d59e2a453866eb9cfc35acf553795233
Danielle Burella <tuj82439@temple.edu>
2020-03-18 14:11:34 -0400

    enabled login through login activity; created UI test for login activity

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/LoginActivity.java
@@ -6,10 +6,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+
+import edu.temple.pihomesecuritymobile.models.Response;
 
 public class LoginActivity extends AppCompatActivity {
     SharedPreferences sharePref;
@@ -41,11 +46,51 @@ public class LoginActivity extends AppCompatActivity {
         String user = username.getText().toString().trim();
         String pass = password.getText().toString().trim();
         if(pass.equals("") || user.equals("")){
+            if (user.equals("")) {
+                username.setError("Field is empty");
+            }
+            if (pass.equals("")) {
+                password.setError("Field is empty");
+            }
             Toast.makeText(getApplicationContext(),"Fields have been left empty", Toast.LENGTH_SHORT).show();
             return null;
         }
-        String[] loginForm = {user, pass};
+        Response response;
+        String result = contentManager.selectIDStatement("UserAccounts", "UserID, AccountID, Username, UserPassword, MasterUserFlag", "Username", "'" + user + "'");
+        Log.d("LOOKUP_USER", "" + result);
+        //put result into a response object
+        response = contentManager.makeResponse(result);
+        if (response.getStatusCode()==contentManager.records_not_exist) {
+            username.setError("Incorrect password or username");
+            password.setError("Incorrect password or username");
+            Toast.makeText(getApplicationContext(),"Incorrect password or username", Toast.LENGTH_SHORT).show();
+            return null;
+        } else if (response==null) {
+            Log.d("null", "lookup response is null; error");
+            return null;
+        }
+        String userID;
+        String accountID;
+        try {
+            if(!pass.equals(response.getBody().getString("UserPassword"))){
+                password.setError("Incorrect password or username");
+                username.setError("Incorrect password or username");
+                Toast.makeText(getApplicationContext(),"Incorrect password or username", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+            userID = response.getBody().getString("UserID");
+            accountID = response.getBody().getString("AccountID");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        String[] loginForm = {user, pass, accountID};
         Toast.makeText(getApplicationContext(),"Logging in...", Toast.LENGTH_SHORT).show();
+        //update last login for user
+        result = contentManager.updateStatement("UserAccounts", "LastLogin", "current_timestamp()", "UserID", userID);
+        Log.d("UPDATE_DATE", "" + result);
+
         return loginForm;
     }
 }
