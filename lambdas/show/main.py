5143933b2a36476bd7cfd88d66dcf9c9e36be4e5
danibtemp <54814629+danibtemp@users.noreply.github.com>
2020-03-19 17:24:19 -0400

    Update main.py

--- lambdas/show/main.py
+++ lambdas/show/main.py
@@ -1,15 +1,14 @@
 # A lambda function to interact with AWS RDS MySQL
-# retrieve a specific record from a table in the database
 
 import pymysql
 import sys
 
-REGION = 'region'
+REGION = 'us-east-1'
 
-rds_host  = "rds_host"
-name = "name"
-password = "password"
-db_name = "database"
+rds_host  = "my-pi-database.cxfhfjn3ln5w.us-east-2.rds.amazonaws.com"
+name = "read_user"
+password = "temple123"
+db_name = "mypidb"
 
 def lambda_handler(event, context):
     """
@@ -23,20 +22,40 @@ def lambda_handler(event, context):
         columns = event['columns']
         columnMatch = event['columnMatch']
         valueMatch = event['valueMatch']
-        query = "select %s from %s where %s = %s" % (columns, table, columnMatch, valueMatch)
+        #fix the valueMatch to have apostrophes if it does not or SQL statement may fail
+        if valueMatch[0]!="'":
+            tempValue = "'" + valueMatch + "'"
+            valueMatch = tempValue
+        #if column we are matching is HomeAccountAddress for user login, we will change our syntax to LIKE instead of =
+        if columnMatch == "HomeAccountAddress":
+            tempValue = valueMatch[:-1]
+            tempValue += "%'"
+            valueMatch = tempValue
+            query = "select %s from %s where %s LIKE %s" % (columns, table, columnMatch, valueMatch)
+            #print(query)
+        else:
+            query = "select %s from %s where %s = %s" % (columns, table, columnMatch, valueMatch)
         try:
             cur.execute(query)
         except Exception as e:
             cur.close()
-            print(str(e))
             return {
+                "statusCode": 413,
                 "error" : str(e)
             }
         cols = cur.description 
         result = [{cols[index][0]:col for index, col in enumerate(value)} for value in cur.fetchall()]
         cur.close()
-        return {
+        if len(result)>0:
+            return {
             'statusCode' : 200,
             'message': "Retrieved records successfully",
             'body' : str(result)
-        }
+            }
+        else:
+            return {
+            'statusCode' : 411,
+            'message': "No records found",
+            'body' : str(result)
+            }
+        
