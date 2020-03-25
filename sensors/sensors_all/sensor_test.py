c480de84efd359f10212b6d531d695883dd41731
Luke-Jakielaszek <tug52339@temple.edu>
2020-03-25 07:10:54 -0700

    Added outline for sensor manager

--- sensors/sensors_all/sensor_test.py
+++ sensors/sensors_all/sensor_test.py
@@ -4,8 +4,20 @@ from microphone import Microphone
 from camera import Camera
 
 if __name__ == '__main__':
-    sm = Sensor_Manager()
+    '''
+    Handles upload of detection files to S3
+    Preprocesses sensor data
+    Triggers camera
+    continuously records mic and ultra in background
+    lock itself on data trigger return, waits for signal from server to start again
+    '''
+
+    sm = Sensor_Manager() # takes in acc iD
     sm.add_sensor(Microphone(duration=60, frequency=44100))
-    #sm.add_sensor(Camera(duration=60, frequency=1))
+    sm.add_sensor(Camera(duration=60, frequency=1))
     sm.connect_all()
     sm.initiate_all()
+
+
+    # (ultra_bucket_filename[], mic_bucket_filename[], cam_bucket_filename[],
+    # trigger_sensor_type, face_match_flag, incident_id)
