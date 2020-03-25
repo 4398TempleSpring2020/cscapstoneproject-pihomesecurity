d869d4b00475615bb1cf38d6b5de8ad5d75c6ed9
Dishant Patel <tug02945@temple.edu>
2020-03-25 14:54:27 -0400

    added client files to mobile app

---
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ClientThread.java
@@ -0,0 +1,16 @@
+package edu.temple.pihomesecuritymobile;
+
+import android.util.Log;
+
+public class ClientThread extends Thread{
+    private Client client;
+
+    public ClientThread(Client c) {
+        this.client = c;
+    }
+
+    public void run(){
+        Log.d("client_test","Starting Server Thread...");
+        client.start();
+    }
+}
