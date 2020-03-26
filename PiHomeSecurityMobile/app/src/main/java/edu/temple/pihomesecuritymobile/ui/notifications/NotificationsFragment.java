c20ce621784a747fe7366308b586016493796003
Danielle Burella <tuj82439@temple.edu>
2020-03-26 14:46:19 -0400

    Added button for photo upload and added camera functionality

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/notifications/NotificationsFragment.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/notifications/NotificationsFragment.java
@@ -1,13 +1,22 @@
 package edu.temple.pihomesecuritymobile.ui.notifications;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -22,6 +31,8 @@ public class NotificationsFragment extends Fragment {
     private NotificationsViewModel notificationsViewModel;
     Context parent;
     onFragListener mList;
+    final private int CAMERA_REQUEST_CODE = 22;
+    final private int CAMERA_INTENT_CODE = 23;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -57,7 +68,60 @@ public class NotificationsFragment extends Fragment {
                 }
             }
         });
+        Button button = root.findViewById(R.id.buttonPhotos);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(parent, AlertDialog.THEME_TRADITIONAL)
+                        .setTitle("Photo Upload")
+                        .setMessage("Choose where to upload photo from")
+                        //.setNeutralButton("Cancel", null)
+                        .setNegativeButton("Gallery", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("Camera", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (parent.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                                    requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                                } else {
+                                    openCamera();
+                                }
+                            }
+                        })
+
+                        .show();
+            }
+        });
         return root;
+    }
+
+    public void openCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_INTENT_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(parent, "Camera permission granted", Toast.LENGTH_LONG).show();
+                openCamera();
+            } else {
+                Toast.makeText(parent, "Camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_INTENT_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+        }
     }
 
     @Override
