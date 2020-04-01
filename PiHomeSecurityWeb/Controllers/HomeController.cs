3704cfc46335fcb316e7291ccc593f21fca58c3e
mullajg <mullajg@gmail.com>
2020-03-31 21:38:33 -0400

    Now matches the updated incident table

---
+++ PiHomeSecurityWeb/Controllers/HomeController.cs
@@ -0,0 +1,158 @@
+﻿                          using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PiHomeSecurityWeb.Models;
+
+namespace PiHomeSecurityWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(HomeAccount account)
+        {
+            using(mypidbContext db = new mypidbContext())
+            {
+                HomeAccount loggedInAccount = db.HomeAccount.Where(x => x.AccountUsername == account.AccountUsername && x.AccountPassword == account.AccountPassword).FirstOrDefault();
+                if(loggedInAccount == null)
+                {
+                    ViewBag.Message = "Incorrect username or password, please try again";
+                    return View();
+                }
+                else
+                {
+                    //Save user info to session
+                    HttpContext.Session.SetInt32("Id", loggedInAccount.AccountId);
+                    HttpContext.Session.SetString("Username", loggedInAccount.AccountUsername);
+
+                    return RedirectToAction("UserTable");
+                }
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Emplogon()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Emplogon(Employee emp)
+        {
+            using (mypidbContext db = new mypidbContext())
+            {
+                Employee loggedInEmployee = db.Employee.Where(x => x.EmployeeId == emp.EmployeeId && 
+                                                                x.EmployeeUsername == emp.EmployeeUsername && 
+                                                                x.EmployeePassword == emp.EmployeePassword).FirstOrDefault();
+                if (loggedInEmployee == null)
+                {
+                    ViewBag.Message = "Incorrect credentials, please try again";
+                    return View();
+                }
+                else
+                {
+                    //Save user info to session
+                    HttpContext.Session.SetInt32("Id", loggedInEmployee.EmployeeId);
+                    HttpContext.Session.SetString("Username", loggedInEmployee.EmployeeUsername);
+
+                    return RedirectToAction("EmpTable");
+                }
+            }
+        }
+        public IActionResult UserTable()
+        {
+            ViewBag.Id = HttpContext.Session.GetInt32("Id");
+            ViewBag.Username = HttpContext.Session.GetString("Username");
+            return View();
+        }
+
+        public IActionResult EmpTable()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Logoff()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logoff(HomeAccount account)
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(HomeAccount account)
+        {
+            try
+            {
+                using (mypidbContext db = new mypidbContext())
+                {
+                    db.HomeAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ViewBag.Message = account.EmailAddress + " successfully registered";
+            }
+            catch
+            {
+                ViewBag.message = "Unable to register user, please try again.";
+            }
+            return View();
+        }
+
+        public IActionResult Accounts()
+        {
+            ViewBag.Id = HttpContext.Session.GetInt32("Id");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PiHomeSecurityWeb/wwwroot/js/TableBuilder.js
@@ -0,0 +1,122 @@
+﻿var TableBuilder = {};
+
+TableBuilder.incidents = function (data, id) {
+    var keysToShow = ["incidentId", "dateRecorded", "badIncidentFlag", "lastAccessed", "adminComments",
+        "deletionBlockFlag", "microphonePath", "imagePaths", "friendlyMatchFlag", "ultrasonicPath"];
+
+    var keysToDisplay = ["Incident Id", "Date Recorded", "Bad Incident Flag", "Last Accessed", "Admin Comments",
+        "Deletion Block Flag", "Microphone Path", "Image Path", "Friendly Match Flag", "Ultrasonic Path"];
+
+    console.log("Keys: " + Object.keys(data[0]));
+    console.log("Values: " + Object.values(data[0]));
+
+    var putTableHere = document.getElementById("tableHere");
+    putTableHere.innerHTML = "";
+    var tableEle = document.createElement("table");
+    putTableHere.appendChild(tableEle);
+
+
+    var tableHead = document.createElement("thead");
+    tableEle.appendChild(tableHead);
+
+    var headerRow = document.createElement("tr");
+    tableHead.appendChild(headerRow);
+
+    for (var i = 0; i < keysToShow.length; i++) {
+        var headerItem = document.createElement("th");
+        headerItem.innerHTML = keysToDisplay[i];
+        headerRow.appendChild(headerItem);
+    }
+
+    var tableBody = document.createElement("tbody");
+    tableEle.appendChild(tableBody);
+
+    for (var i = 0; i < data.length; i++) {
+        if (id == data[i].accountId) {
+            var tableRow = document.createElement("tr");
+            tableBody.appendChild(tableRow);
+            for (var j = 0; j < Object.values(data[i]).length; j++) {
+                if (keysToShow.includes(Object.keys(data[i])[j])) {
+                    console.log("Adding data to table:");
+                    console.log("Row " + i + ", Key: " + Object.keys(data[i])[j] + ": " + Object.values(data[i])[j]);
+                    var tableData = document.createElement("td");
+                    if (Object.keys(data[i])[j] == "imagePaths") {
+                        var tableLink = document.createElement("img");
+                        tableLink.src = TableBuilder.createImgLink(Object.values(data[i])[j]);
+                        tableLink.style.width = "100px";
+                        tableData.appendChild(tableLink);
+                    }
+                    else {
+                        tableData.innerHTML = Object.values(data[i])[j];
+                    }
+                    tableRow.appendChild(tableData);
+                }
+            }
+        }
+    }
+}
+
+TableBuilder.useraccounts = function (data, id) {
+    var keysToShow = ["userId", "username", "masterUserFlag", "dateCreated", "lastLogin"];
+
+    var keysToDisplay = ["User Id", "Username", "Master User?", "Date Created", "Last Login"];
+
+    console.log("Keys: " + Object.keys(data[0]));
+    console.log("Values: " + Object.values(data[0]));
+
+    console.log("1");
+
+    var putTableHere = document.getElementById("tableHere");
+    putTableHere.innerHTML = "";
+    var tableEle = document.createElement("table");
+    putTableHere.appendChild(tableEle);
+
+    console.log("2");
+
+
+    var tableHead = document.createElement("thead");
+    tableEle.appendChild(tableHead);
+
+    var headerRow = document.createElement("tr");
+    tableHead.appendChild(headerRow);
+
+    console.log("3");
+
+    for (var i = 0; i < keysToShow.length; i++) {
+        var headerItem = document.createElement("th");
+        headerItem.innerHTML = keysToDisplay[i];
+        headerRow.appendChild(headerItem);
+    }
+
+    console.log("4");
+
+    var tableBody = document.createElement("tbody");
+    tableEle.appendChild(tableBody);
+
+    console.log("5");
+
+    for (var i = 0; i < data.length; i++) {
+        console.log("6 outter");
+        console.log("data[i].accountId: " + data[i].accountId);
+        console.log("id: " + id);
+        if (id == data[i].accountId) {
+            var tableRow = document.createElement("tr");
+            tableBody.appendChild(tableRow);
+            for (var j = 0; j < Object.values(data[i]).length; j++) {
+                console.log("6 inner");
+                if (keysToShow.includes(Object.keys(data[i])[j])) {
+                    console.log("Adding data to table:");
+                    console.log("Row " + i + ", Key: " + Object.keys(data[i])[j] + ": " + Object.values(data[i])[j]);
+                    var tableData = document.createElement("td");
+                    tableData.innerHTML = Object.values(data[i])[j];
+                    tableRow.appendChild(tableData);
+                }
+            }
+        }
+    }
+}
+
+TableBuilder.createImgLink = function(urlTail){
+    var urlHead = "https://whateverworks.s3.us-east-2.amazonaws.com/";
+    return (urlHead + urlTail);
+}
