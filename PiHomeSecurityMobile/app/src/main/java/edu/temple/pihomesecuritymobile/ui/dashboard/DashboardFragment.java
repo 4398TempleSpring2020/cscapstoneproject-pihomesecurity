4434af01f49c382396da63fbc3a8f98176dbfe5a
Dishant Patel <tug02945@temple.edu>
2020-04-08 09:25:14 -0400

    added interface functions

--- PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/dashboard/DashboardFragment.java
+++ PiHomeSecurityMobile/app/src/main/java/edu/temple/pihomesecuritymobile/ui/dashboard/DashboardFragment.java
@@ -95,13 +95,15 @@ public class DashboardFragment extends Fragment {
         //TextView adminComm = root.findViewById(R.id.textView4);
         GridView gridView = root.findViewById(R.id.gridViewInc);
         int set = -1;
+
         Button button = root.findViewById(R.id.escalate);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                mList.escalateRsp();
             }
         });
+
         if (dataObj==null && dataArr==null) { //no incidents exist yet
             set = -1;
         } else if(dataObj == null){ //only one incident exists
