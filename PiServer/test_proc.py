744b22660a579256810bbf687e3d4e1cc705a440
LukeJakielaszek <tug52339@temple.edu>
2020-04-14 16:19:46 -0700

    Completed microphone threshold calculation

--- PiServer/test_proc.py
+++ PiServer/test_proc.py
@@ -1,10 +1,14 @@
 from ultra_proc import UltraProc
+from mic_proc import MicProc
 import sys
 
 ultraProc = UltraProc()
+micProc = MicProc()
 
 fname = [sys.argv[1]]
 
-isAnom = ultraProc.isAnomaly(fname)
+#isAnom = ultraProc.isAnomaly(fname)
+isAnom = micProc.isAnomaly(fname)
+
 print(isAnom)
 
