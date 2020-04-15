d2ff1902ad3aa6ed5aa60c646cdb9647a43ce521
jonathan <jonathansanks@gmail.com>
2020-04-15 08:36:02 -0400

    most recent Driver.py

--- PiServer/Driver.py
+++ PiServer/Driver.py
@@ -37,39 +37,3 @@ if __name__ == '__main__':
     message_handler_thread.start()
     #logic_handler_thread = LogicHandlerThread(shared_resources)
     #logic_handler_thread.start()  #comment out if you do not have sensors
-
-    '''shared_resources.q_lock.acquire()
-    while len(message_q) > 0:
-        print(shared_resources.message_q.pop())
-    shared_resources.q_lock.release()''' '''
-            
-            
-            # db_connection.test_integration()
-    
-            ret_dict = ast.literal_eval(
-                "{'microphone': ['123/1585615830.5592604/microphone/audio.wav'], "
-                "'camera': ['123/1585615830.5592604/camera/image_0.jpg', '123/1585615830.5592604/camera/image_1.jpg',"
-                "'123/1585615830.5592604/camera/image_2.jpg', '123/1585615830.5592604/camera/image_3.jpg', "
-                "'123/1585615830.5592604/camera/image_4.jpg', '123/1585615830.5592604/camera/image_5.jpg', "
-                "'123/1585615830.5592604/camera/image_6.jpg', '123/1585615830.5592604/camera/image_7.jpg', "
-                "'123/1585615830.5592604/camera/image_8.jpg', '123/1585615830.5592604/camera/image_9.jpg'],"
-                "'ultrasonic': ['123/1585615830.5592604/ultrasonic/ultra.txt'],"
-                " 'bucket': 'whateverworks', "
-                "'instance_id': '1585615830.5592604',"
-                " 'face_match_flag': False, "
-                "'wasAlert': True, "
-                "'trigger_sensor_type': ['camera']}")
-    
-            # ret_dict = run_everything(4)
-            incident_id = str(ret_dict["instance_id"])
-            face_match_flag = str(ret_dict["face_match_flag"])
-            image_path = ret_dict["camera"]
-            mic_path = ret_dict["microphone"][0]
-            ultrasonic_path = ret_dict["ultrasonic"][0]
-    
-            temp = IncidentData(4, incident_id, 1, image_path, mic_path, ultrasonic_path)
-            print(db_connection.insert_incident_data(temp))
-            # ret_dict = run_everything(123)
-    
-            print('COMPLETED') 
-            '''  # test incident
