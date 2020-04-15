97ccff2cd706f5d1a7cb4c2cf800a6cc8affb0a1
danibtemp <54814629+danibtemp@users.noreply.github.com>
2020-04-15 15:20:32 -0400

    Update main.py
    ran out of sms notifications

--- lambdas/contactPolice/main.py
+++ lambdas/contactPolice/main.py
@@ -3,6 +3,15 @@ import pymysql
 import sys
 from dateutil import tz
 from datetime import datetime
+import base64
+import json
+import os
+import urllib
+from urllib import request, parse
+
+TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
 REGION = 'region'
 
@@ -12,8 +21,6 @@ password = "password"
 db_name = "database"
 
 def lambda_handler(event, context):
-    session = boto3.Session(region_name="us-east-1")
-    sns = session.client('sns')
     result = []
     conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
     homeID = event['homeID']
@@ -53,12 +60,30 @@ def lambda_handler(event, context):
     newDateTime = convert_to_est(str(newDateTime))
     #print(newDateTime)
     incAlertMessage = "A break-in was recorded at home address " + homeAddress + " at " + newDateTime + " EST by home security system PiHomeSecurity. \nPlease investigate immediately."
-    number = "+12679803862"
+    number = os.environ.get("NUMBER")
     #number = incidentPhoneNumber
-    response = sns.publish(
-        PhoneNumber = number,
-        Message =  incAlertMessage
-    )
+    from_number = os.environ.get("FROM_NUMBER")
+    # insert Twilio Account SID into the REST API URL
+    populated_url = TWILIO_SMS_URL.format(TWILIO_ACCOUNT_SID)
+    post_params = {"To": number, "From": from_number, "Body": incAlertMessage}
+
+    # encode the parameters for Python's urllib
+    data = parse.urlencode(post_params).encode()
+    req = request.Request(populated_url)
+
+    # add authentication header to request based on Account SID + Auth Token
+    authentication = "{}:{}".format(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    base64string = base64.b64encode(authentication.encode('utf-8'))
+    req.add_header("Authorization", "Basic %s" % base64string.decode('ascii'))
+
+    try:
+        # perform HTTP POST request
+        with request.urlopen(req, data) as f:
+            print("Twilio returned {}".format(str(f.read().decode('utf-8'))))
+    except Exception as e:
+        # something went wrong!
+        return e
+    
     return {
         "status": "Successfully sent sms alert to users for incident detected"   
     }
