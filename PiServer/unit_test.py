be86da2f880f96ee22844167500da9ac8ee51d77
Luke-Jakielaszek <tug52339@temple.edu>
2020-04-19 19:13:36 -0900

    Integration of camera processing code and unit test

--- PiServer/unit_test.py
+++ PiServer/unit_test.py
@@ -8,14 +8,17 @@ from cam_proc import CamProc
 import os
 import unittest
 from run_sensors import run_sensors
+from camera_proc_help import facial_recognition
+import threading
 
 class TestSensors(unittest.TestCase):
     # ensure running sensors returns a tuple with 4 items, each containing non null information
     def test_running_sensors(self):
+        print('testing running all sensors')
         bucket_name = "mypishield"
         acc_old_id = 11
 
-        # run all sensors
+        # run all sensorsls
         ret_vals = run_sensors(10, acc_old_id, bucket_name)
 
         # check length
@@ -23,98 +26,119 @@ class TestSensors(unittest.TestCase):
 
         # unpack it and check existance
         ret_list, anomaly_dict, instance_id, acc_id = ret_vals
-        self.assertTrue(ret_list not None)
-        self.assertTrue(anomaly_dict not None)
-        self.assertEquals(len(anomaly_dict), 3)
-        self.assertTrue(instance_id not None)
-        self.assertTrue(acc_id not 11)
+        self.assertTrue(not ret_list is None)
+        self.assertTrue(not anomaly_dict is None)
+        self.assertEquals(len(anomaly_dict), 4)
+        self.assertTrue(not instance_id is None)
+        self.assertTrue(acc_id == acc_old_id)
 
     # test image processing script for known output (hostile / friendly)
     def test_image_proc(self):
-        camProc = CamProc()
+        print('Testing image processing')
+        faces = os.listdir("./known_people")
 
-        known = os.listdir("./known_people")
+        known = []
+        for fn in faces:
+            known.append('./known_people/' + fn)
+        
+        # true
+        self.assertTrue(facial_recognition("unknown_people/unknown1.jpg", known))
 
         # true
-        self.assertTrue(CamProc.facial_recognition("unknown_people/unknown1.jpg"), known)
-
-        # true
-        self.assertTrue(CamProc.facial_recognition("unknown_people/unknown2.jpg"), known)
+        self.assertTrue(facial_recognition("unknown_people/unknown2.jpg", known))
 
         # false
-        self.assertTrue(CamProc.facial_recognition("unknown_people/unknown3.jpg", known)
+        self.assertTrue(facial_recognition("unknown_people/unknown3.jpg", known))
 
         # true
-        self.assertTrue(CamProc.facial_recognition("unknown_people/unknown4.jpg"), known)
+        self.assertTrue(facial_recognition("unknown_people/unknown4.jpg", known))
 
         
     # test that we recognize far anomalies for ultra
     def test_ultra_proc_far(self):
+        print('Testing ultra processing far')
+
         ultraProc = UltraProc()
-        fname = "./data/ultra/far_anomaly.txt"
+        fname = ["./data/ultra/far_anomaly.txt"]
         isAnom = ultraProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, True)
 
     # test that we recognize medium anomalies for ultra
     def test_ultra_proc_med(self):
+        print('Testing ultra processing med')
+
         ultraProc = UltraProc()
-        fname = "./data/ultra/medium_anomaly.txt"
+        fname = ["./data/ultra/medium_anomaly.txt"]
         isAnom = ultraProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, True)
 
     # test that we recognize close anomalies for ultra
     def test_ultra_proc_close(self):
+        print('Testing ultra processing close')
+
         ultraProc = UltraProc()
-        fname = "./data/ultra/close_anomaly.txt"
+        fname = ["./data/ultra/close_anomaly.txt"]
         isAnom = ultraProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, True)
 
     # test that we do not recognize static anomalies for ultra
-    def test_ultra_proc_close(self):
+    def test_ultra_proc_static(self):
+        print('Testing ultra processing static')
+
         ultraProc = UltraProc()
-        fname = "./data/ultra/static_anomaly.txt"
+        fname = ["./data/ultra/static_anomaly.txt"]
         isAnom = ultraProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, False)
 
     # test that we do not recognize distant walking anomalies for ultra
-    def test_ultra_proc_close(self):
+    def test_ultra_proc_offscreen(self):
+        print('Testing ultra processing offscreen')
+
         ultraProc = UltraProc()
-        fname = "./data/ultra/offscreenwalking.txt"
+        fname = ["./data/ultra/offscreenwalking.txt"]
         isAnom = ultraProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, False)
 
     # test that we recognize close anomalies for mic
     def test_mic_proc_close(self):
-        ultraProc = MicProc()
-        fname = "./data/mic/close_talking.wav"
+        print('Testing mic processing close')
+
+        micProc = MicProc()
+        fname = ["./data/mic/close_talking.wav"]
         isAnom = micProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, True)
 
     # test that we recognize loud clapping for mic
     def test_mic_proc_loud(self):
-        ultraProc = MicProc()
-        fname = "./data/ultra/medium_anomaly.wav"
+        print('Testing mic processing loud')
+
+        micProc = MicProc()
+        fname = ["./data/mic/loud_clapping.wav"]
         isAnom = micProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, True)
 
     # test that we do not recognize silent fo mic
     def test_mic_proc_silent(self):
-        ultraProc = MicProc()
-        fname = "./data/mic/silent.wav"
-        isAnom = ultraProc.isAnomaly(fname)
+        print('Testing mic processing silent')
+
+        micProc = MicProc()
+        fname = ["./data/mic/silent.wav"]
+        isAnom = micProc.isAnomaly(fname)
 
         self.assertEquals(isAnom, False)
 
     # test that exactly twice the number of files downloaded are in the downloads folder
     # (i.e. each face has a corresponding meta file)
     def test_s3_user_face_download_count(self):
+        print('Testing s3 download count')
+
         client = S3_Client()
         files = client.get_user_face_files("mypishield", 11)
         num_files = len(files)
@@ -125,6 +149,8 @@ class TestSensors(unittest.TestCase):
 
     # test that each meta file exists after downloading faces
     def test_s3_user_face_download_meta(self):
+        print('Testing s3 download meta')
+
         client = S3_Client()
         files = client.get_user_face_files("mypishield", 11)
 
@@ -137,6 +163,8 @@ class TestSensors(unittest.TestCase):
 
     # test that each face file exists after downloading faces
     def test_s3_user_face_download(self):
+        print('Testing s3 download existence')
+
         client = S3_Client()
         files = client.get_user_face_files("mypishield", 11)
 
