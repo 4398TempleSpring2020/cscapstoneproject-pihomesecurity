bfc3cd1d93d541a0026a260e07a800be08bf7b57
LukeJakielaszek <tug52339@temple.edu>
2020-04-23 18:09:08 -0700

    Added unit tests for mic recording audio file, ultra recording distance file, and camera taking image file

--- PiServer/cameracode.py
+++ PiServer/cameracode.py
@@ -1,14 +1,16 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
 
-# Take out this if no rotation is needed
-camera.rotation = 180
+def test_cam():
+    camera = PiCamera()
+    
+    # Take out this if no rotation is needed
+    camera.rotation = 180
+    
+    camera.start_preview()
+    sleep(5)
 
-camera.start_preview()
-sleep(5)
-
-# This code takes the picture
-camera.capture('./image.jpg')
-camera.stop_preview()
+    # This code takes the picture
+    camera.capture('./test_image.jpg')
+    camera.stop_preview()
